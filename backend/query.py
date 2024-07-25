21d6ba68082d7492aae1c360807f6e4f2b041c86
Taiki Yamashita <taikiy49@gmail.com>
2024-07-24 20:12:03 -1000

    more done

--- backend/query.py
+++ backend/query.py
@@ -26,12 +26,12 @@ def load_json_file(file_path):
         data = json.load(f)
     return data
 
-try:
-  conversation_data = load_json_file('pdf_data.json')
-  chat_session = model.start_chat(history=conversation_data)
-except FileNotFoundError:
-  print('json file does not exist! we can fix this later...')
-
 def run_query(prompt):
-  response = chat_session.send_message("Given just all the information I fed you earlier" + prompt + "ONLY give me answers that are related to the topic and keep it short!")
-  return response.text.strip()
+  try:
+    conversation_data = load_json_file('pdf_data.json')
+    print(conversation_data)
+    chat_session = model.start_chat(history=conversation_data)
+    response = chat_session.send_message("Given just all the information I fed you earlier" + prompt + "ONLY give me answers that are related to the topic and keep it short!")
+    return response.text.strip()
+  except FileNotFoundError:
+    print('json file does not exist! we can fix this later...')
